20fed078c2ac35a9280c8e0aa1071960c8f390da
lionking0105 <alexanderfoster0105@gmail.com>
2023-05-08 16:07:29 -0500

    Auth UI

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 export default function Home() {
   return (
     <div className='flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
@@ -14,7 +15,7 @@ export default function Home() {
           Sign in to your account
         </h2>
       </div>
-      {/* Auth */}
+      <AuthForm />
     </div>
   );
 }
